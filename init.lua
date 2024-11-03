833fcee37dfb7a3eb969d350d4b7c90e7560dd4a
Raul Sauco <sauco.raul@gmail.com>
2024-11-03 06:54:05 +0100

    Update conform fallback parameter

--- init.lua
+++ init.lua
@@ -649,7 +649,7 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
         desc = '[F]ormat buffer',
@@ -665,9 +665,15 @@ require('lazy').setup({
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
         return {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_format = lsp_format_opt,
         }
       end,
       formatters_by_ft = {
@@ -677,8 +683,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier', stop_after_first = true } },
-        scss = { { 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        scss = { 'prettier' },
       },
     },
   },
